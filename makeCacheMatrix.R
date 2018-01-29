ff00059bfa3d4cd4613e90112bfbb556d868824e
trinath kondra <trinath.kondra@gmail.com>
2018-01-28 22:34:56 -0500

    First commit

---
+++ makeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
